4b10e52c98f8f223d600736cc3412b08ec9db844
Josh Kim <joshjayk@live.com>
2016-01-07 16:56:03 -0500

    Added a dictionary of titles and users that commented.

--- Processor.py
+++ Processor.py
@@ -1,22 +1,26 @@
 import praw
 
-# Initialize the API wrapper
-r = praw.Reddit(user_agent='Commenter Processor')
+def main():
+	# Initialize the API wrapper
+	r = praw.Reddit(user_agent='Commenter Processor')
+	# This is how to query posts within a specific subreddit
+	submissions = r.get_subreddit('news').search("Iraq",sort="hot",limit=5)
+	userList = {}
+	# Loop through all the search results
+	for post in submissions:
+		title = str(post).split(":: ", 1)[1]
+		userList.setdefault(title, [])
+		# Loop through all the comments within a post
+		for comment in post.comments:
+			# Check that a comment is not a MoreComments type
+			if(type(comment) is not praw.objects.MoreComments):
+				# Get the Redditor's name and print it
+				author = comment.author
+				# Make sure the Redditor is not deleted
+				if(author != None):
+					if (author.name not in userList[title]):
+						userList[title].append(author.name)
+	print(userList)
 
-# This is how to query posts within a specific subreddit
-submissions = r.get_subreddit('news').search("Iraq",sort="top",limit=10)
-
-# Loop through all the search results
-for post in submissions:
-	
-	# Loop through all the comments within a post
-	for comment in post.comments:
-
-		# Check that a comment is not a MoreComments type
-		if(type(comment) is not praw.objects.MoreComments):
-
-			# Get the Redditor's name and print it
-			author = comment.author
-			# Make sure the Redditor is not deleted
-			if(author != None):
-				print(author.name)
+if __name__ == "__main__":
+	main()
