848a800845c463e822cfcad997f71977de84934d
Josh Kim <joshjayk@live.com>
2016-01-08 17:21:19 -0500

    Optimized string formatting and output. Also switched the comment parsing to use the replace_more_comments function from praw, to avoid the AssertionError in the recursion.

--- Processor.py
+++ Processor.py
@@ -16,11 +16,11 @@ def getAllComments(seedComments):
 	return allComments
 
 def main():
-	output = open('output.txt', 'w')
 	subList = []
 	keyList = []
 	searchLimit = 0
 	sortType = ""
+	output = open('output.txt', 'w')
 	with open('input.txt', 'r') as input:
 		for line in input:
 			category, contents = line.split(": ", 1)
@@ -28,34 +28,38 @@ def main():
 			contents[-1] = contents[-1].rstrip('\n')
 			if category == "SUBREDDITS":
 				subList = contents
-				print("SUBREDDITS: "+str(contents))
+				print("SUBREDDITS: %s" % contents)
 			elif category == "KEYWORDS":
 				keyList = contents
-				print("KEYWORDS: "+str(contents))
+				print("KEYWORDS: %s" % contents)
 			elif category == "LIMIT":
 				searchLimit = contents
-				print("LIMIT: "+str(contents))
+				print("LIMIT: %s" % contents)
 			elif category == "TYPE":
 				sortType = contents
-				print("TYPE: "+str(contents))
+				print("TYPE: %s" % contents)
 	# Initialize the API wrapper
 	r = praw.Reddit(user_agent='Commenter Processor')
 	# This is how to query posts within a specific subreddit
 	for subreddit in subList:
 		for keyword in keyList:
-			submissions = r.get_subreddit(subreddit).search(keyword,sort=str(sortType[0]),limit=int(searchLimit[0]))
+			submissions = r.get_subreddit(subreddit).search(keyword,sort=sortType[0],limit=int(searchLimit[0]))
 			userList = {}
 			userCount = {}
 			totalUsers = 0
+			limTracker = 1
 			# Loop through all the search results
 			for post in submissions:
+				print("SUBREDDIT: %s, KEYWORD: %s, COUNT: %d" % (subreddit, keyword, limTracker))
+				limTracker += 1
 				title = str(post).split(":: ", 1)[1]
 				# For some reason my windows command prompt didn't agree with printing apostrophes.
 				# My fix was to add the encode below. Apparently it's bad. ¯\_(ツ)_/¯
 				# print(title.encode("utf-8"))
 				userList.setdefault(title, [])
 				# Loop through all the comments within a post
-				for comment in getAllComments(post.comments):
+				post.replace_more_comments(limit=None, threshold=0)
+				for comment in praw.helpers.flatten_tree(post.comments):
 					# Get the Redditor's name and print it
 					author = comment.author
 					# Make sure the Redditor is not deleted
@@ -68,11 +72,9 @@ def main():
 						totalUsers += 1
 					else:
 						userCount[user] += 1
-			output.write("SUBREDDIT: " + str(subreddit) + "\n")
-			output.write("KEYWORD: " + str(keyword) + "\n")
+			output.write("SUBREDDIT: %s\nKEYWORD: %s\nTOTAL USERS: %d\n" % (subreddit, keyword, totalUsers))
 			for key,value in userCount.items():
-				output.write(str(key) + ": " + str(value) + "\n")
-			output.write(str(totalUsers) + "\n\n")
+				output.write("%20s: %2s\n" % (key, value))
 	output.close()
 if __name__ == "__main__":
 	main()
